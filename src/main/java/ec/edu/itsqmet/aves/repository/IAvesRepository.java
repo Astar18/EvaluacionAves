fe93e556727bc90aa5213728b12a43b3f1368eea
Astar18 <aramirez@itsqmet.edu.ec>
2022-12-17 18:08:44 -0500

    agregar clase repository

--- src/main/java/ec/edu/itsqmet/aves/repository/IAvesRepository.java
+++ src/main/java/ec/edu/itsqmet/aves/repository/IAvesRepository.java
@@ -1,5 +1,7 @@
 package ec.edu.itsqmet.aves.repository;
 
-public class IAvesRepository {
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
+public class IAvesRepository extends JpaRepository<AvesDTO, Integer>{
 
 }
