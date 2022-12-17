be4ec5b26c2b7c6433bc617375ffd7731a5ae853
Astar18 <aramirez@itsqmet.edu.ec>
2022-12-17 18:15:31 -0500

    metodo prueba ave singleton

--- src/test/java/com/example/demo/EvaluacionAvesApplicationTests.java
+++ src/test/java/com/example/demo/EvaluacionAvesApplicationTests.java
@@ -13,6 +13,12 @@ class EvaluacionAvesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println(AvesSingleton.getAves().size());
+		//CountryCatalogDTO abc = new AvesDTO();
+		//System.out.println(abc.imprimirMensaje());
+		//System.out.println("Hello World");
+		//System.out.println(AvesRepository.count());
+		//System.out.println(AvesSingleton.getCountries(AvesService).size());
 		
 		AvesApplication aves = new AvesApplication(1, "acuáticas");
 		AvesApplication aves1 = new AvesApplication(2, "de rapiña");
